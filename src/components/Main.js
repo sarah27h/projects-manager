6fd58b89c8868af7be082cc568ff0d73e4158acc
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-01-01 20:24:28 +0200

    Feat: create <Main />

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Nav from './layout/Nav'
+
+class Main extends Component {
+    render () {
+        return (
+            <div>
+                <Nav />
+                Main
+            </div>
+        )
+    }
+    
+}
+
+export default Main;
