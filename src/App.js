c6c67c8cf132e1bc676938ae46b9f8b2911de752
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-01-05 22:02:02 +0200

    refactor: remove BrowserRouter from App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import {BrowserRouter} from 'react-router-dom';
 import Main from './components/Main'
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Main />
-        </div>
-      </BrowserRouter>
+      <div className="App">
+        <Main />
+      </div>
     );
   }
 }
