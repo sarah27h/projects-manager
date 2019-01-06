2d4e31909ca4acbd3ac8e899856fecc496e5751b
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-01-06 20:51:52 +0200

    Feat: setup a Route for <SignIn />

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,7 @@ import Navbar from './layout/Navbar';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Dashboard from './dashboard/Dashboard';
 import ProjectDetails from './projects/ProjectDetails';
+import SignIn from './auth/SignIn';
 
 class Main extends Component {
     render () {
@@ -12,7 +13,8 @@ class Main extends Component {
                     <Navbar />
                     <Switch>
                         <Route exact path="/" component={Dashboard} />
-                        <Route path="/project/:id" component={ProjectDetails} />                        
+                        <Route path="/project/:id" component={ProjectDetails} />
+                        <Route path="/signin" component={SignIn} />
                     </Switch>
                 </div>
             </BrowserRouter>
