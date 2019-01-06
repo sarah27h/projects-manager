4d1ac01e51b4a73e8cc493c993505c9e02dede85
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-01-06 17:22:59 +0200

    Feat: create <ProjectSummary />

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+    return (
+        <div className="card">
+            <div className="card-content">
+                <span className="card-title">project title</span>
+                <p>posted by Salma Allam</p>
+                <p>1st Octoder, 10am</p>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectSummary;
