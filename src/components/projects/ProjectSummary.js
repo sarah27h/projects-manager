f17cb9d2560a1caada2615f1cad7c8c7fcdfe5e5
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-01-06 17:23:54 +0200

    Fix: typo

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -6,7 +6,7 @@ const ProjectSummary = () => {
             <div className="card-content">
                 <span className="card-title">project title</span>
                 <p>posted by Salma Allam</p>
-                <p>1st Octoder, 10am</p>
+                <p>1st October, 10am</p>
             </div>
         </div>
     )
