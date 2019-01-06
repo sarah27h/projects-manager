71f5dce1bb6c5d2fec457893f863e1a612a03e99
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-01-06 18:21:36 +0200

    Feat: setup a Route for <ProjectDetails />

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Navbar from './layout/Navbar';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Dashboard from './dashboard/Dashboard';
+import ProjectDetails from './projects/ProjectDetails';
 
 class Main extends Component {
     render () {
@@ -11,6 +12,7 @@ class Main extends Component {
                     <Navbar />
                     <Switch>
                         <Route path="/" component={Dashboard} />
+                        <Route path="/project/:id" component={ProjectDetails} />                        
                     </Switch>
                 </div>
             </BrowserRouter>
