c12d54e00a011dd88e9e874b9f64f5d8aaeb86db
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-01-06 17:56:39 +0200

    Feat: create <ProjectDetails />

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ProjectDetails = () => {
+    return(
+        <div className="card">
+            <div className="card-content">
+                <span className="card-title">project title</span>
+                <p>I am a very simple card. I am good at containing small bits of information.
+                    I am convenient because I require little markup to use effectively.
+                </p>
+            </div>
+            <div class="card-action">
+                <p>posted by Salma Allam</p>
+                <p>1st October, 10am</p>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectDetails;
