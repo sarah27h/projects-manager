af7031c25ac2dee420eac4223a9d36f345dfa46f
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-01-06 21:25:11 +0200

    Feat: setup a Route for <SignUp />

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Dashboard from './dashboard/Dashboard';
 import ProjectDetails from './projects/ProjectDetails';
 import SignIn from './auth/SignIn';
+import SignUp from './auth/SignUp';
 
 class Main extends Component {
     render () {
@@ -15,6 +16,7 @@ class Main extends Component {
                         <Route exact path="/" component={Dashboard} />
                         <Route path="/project/:id" component={ProjectDetails} />
                         <Route path="/signin" component={SignIn} />
+                        <Route path="/signup" component={SignUp} />                        
                     </Switch>
                 </div>
             </BrowserRouter>
