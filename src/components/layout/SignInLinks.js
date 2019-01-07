9997cc22cb6f2375743c5fd6b6677928dcf88223
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-01-07 19:09:01 +0200

    Feat: adjust Navbar links to show correct component

--- src/components/layout/SignInLinks.js
+++ src/components/layout/SignInLinks.js
@@ -4,9 +4,9 @@ import {NavLink} from 'react-router-dom';
 const SignInLinks = () => {
     return (
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><NavLink to="">New Project</NavLink></li>
+            <li><NavLink to="/createproject">New Project</NavLink></li>
             <li><NavLink to="">Log out</NavLink></li>
-            <li><NavLink to="" className="btn btn-floating">SH</NavLink></li>                 
+            <li><NavLink to="/" className="btn btn-floating">SH</NavLink></li>
         </ul>
     )
 }
--- src/components/layout/SignOutLinks.js
+++ src/components/layout/SignOutLinks.js
@@ -4,8 +4,8 @@ import {NavLink} from 'react-router-dom';
 const SignOutLinks = () => {
     return (
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><NavLink to="">Signup</NavLink></li>
-            <li><NavLink to="">Login</NavLink></li>
+            <li><NavLink to="/signup">Signup</NavLink></li>
+            <li><NavLink to="/signin">Login</NavLink></li>
         </ul>
     )
 }
