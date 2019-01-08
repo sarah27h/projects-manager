e6e56eb74855921d31da1589542a4b8f711eedaf
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-01-08 19:10:29 +0200

    Feat: combine reducers into rootReducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,5 +1,14 @@
-const rootReducer = () => {
+/* 
+    import reducers to combine into 
+    one reducer using combineReducers 
+*/
+import authReducer from './authReducer';
+import projectReducer from './projectReducer';
+import { combineReducers } from 'redux';
 
-}
+const rootReducer = combineReducers({
+    auth: authReducer,
+    project: projectReducer
+})
 
 export default rootReducer;
