91eba0e877147437c0dae9ae0c1a7a35687ec4d1
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-01-08 19:13:18 +0200

    Feat: bind redux with our react app using <Provider/>

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
+/* use Provide component to bind redux with our react app*/
+import Provider from 'react-redux';
 
 /* create a store and pass rootReducer into it */
 const store = createStore(rootReducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}> <App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
