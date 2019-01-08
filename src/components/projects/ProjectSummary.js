c422d9226b4de13ec49f2c2cd8ffa27c502fda87
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-01-08 22:00:52 +0200

    Feat: receive those props using destructuring

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
     return (
         <div className="card">
             <div className="card-content">
-                <span className="card-title">project title</span>
+                <span className="card-title">{project.title}</span>
                 <p>posted by Salma Allam</p>
                 <p>1st October, 10am</p>
             </div>
