853530db68192853c224e4c81414d5266504b373
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-01-08 21:16:13 +0200

    Feat: add a dummy data in projectReducer
    so projectReducer will return this data
    then all info is going to be stored on state
    on project property of rootReducer

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -2,7 +2,13 @@
     add initState to pass as default value 
     so when app run first time use this as init value
 */
-const initState = {}
+const initState = {
+    projects: [
+        {id: '1', title: 'Realstate project', content: 'I will use Reactjs, Redux, and a third party API to build a responsive front end for a real estate portal with an interactive map to view the placement of listings.'},
+        {id: '2', title: 'React Get Weather app', content: 'a single page application from scratch to work as an interactive weather app built using React.js.'},
+        {id: '3', title: 'Realstate project', content: 'I will use Reactjs, Redux, and a third party API to build a responsive front end for a real estate portal with an interactive map to view the placement of listings.'}
+    ]
+}
 
 const projectReducer = (state=initState, action) => {
     return state;
