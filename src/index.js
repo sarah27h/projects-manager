9c202cfe3beb75af395aedb9cbabc68f6ab5ed1e
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-01-21 21:19:29 +0200

    Feat: connect redux store with firebase
    1- using 2 packages redux-firestore, react-redux-firebase
      --> to provide binding to firebase, firestore
    2- using extra arguments (getFirestore, getFirebase) for thunk
      --> to use firebase and firestore services inside action creator
    3- include reactReduxFirebase, reduxFirestore (store enhancer)
      --> to connect project config file with our app

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 // to apply thunk we need applyMiddleware
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 /* use Provide component to bind redux with our react app*/
 import { Provider } from 'react-redux';
@@ -12,16 +12,27 @@ import { Provider } from 'react-redux';
 /*import thunk middleware*/
 import thunk from 'redux-thunk';
 
-/* import extra arguments for thunk to use inside action creator  */
-import { getFirestore } from 'redux-firestore';
-import { getFirebase } from 'react-redux-firebase';
+// import extra arguments (getFirestore, getFirebase) for thunk to use inside action creator
+// include reactReduxFirebase, reduxFirestore (store enhancer) 
+// to connect project config file with our app
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import fbConfig from './cofig/fbConfig';
 
 // create a store and pass rootReducer into it
 // createStore second arguments is the "store enhancer"
 // use applyMiddleware to apply the thunk middleware to our store.
 // use withExtraArgumet to pass extra arguments for thunk function
 // to provide binding to firebase service, firestore db 
-const store = createStore(rootReducer, applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })));
+const store = createStore(rootReducer, 
+    // use compose to combine store enhancers together
+    // pass fbConfig file to firebase enhancer to connect project config file with our app
+    compose(
+        applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
+        reactReduxFirebase(fbConfig),
+        reduxFirestore(fbConfig)
+    )
+);
 
 ReactDOM.render(<Provider store={store}> <App /></Provider>, document.getElementById('root'));
 
