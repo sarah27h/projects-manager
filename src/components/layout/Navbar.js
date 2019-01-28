9e7d6998e58096b19ed1882bbdaa3055b8258530
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-01-28 22:36:13 +0200

    Feat: Tracking Auth Status to show sign links

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,14 +4,17 @@ import SignInLinks from './SignInLinks';
 import SignOutLinks from './SignOutLinks';
 import { connect } from 'react-redux'; // connect to our redux state
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const { auth } = props;
+    // conditionally show sign links components based on isEmpty property
+    const links = auth.isEmpty ? <SignOutLinks /> :   <SignInLinks />; 
     return (
         <nav>
             <div className="nav-wrapper blue-grey darken-4">
                 <div className="container">
                     <Link to="/" className="brand-logo">Project Manager</Link>
-                    <SignInLinks />
-                    <SignOutLinks />
+    
+                    {links}
                 </div>
             </div>
         </nav>
@@ -19,11 +22,15 @@ const Navbar = () => {
     
 }
 
-
+// use authentication status from state
+// add auth to component props
+// to use it conditionally show sign links components based on it
+// state.firebase.auth.isEmpty: true >> logout
+//state.firebase.auth.isEmpty: false >> login
 const mapStatetoProps = (state) => {
     console.log(state);
     return {
-
+        auth: state.firebase.auth
     }
 }
 
