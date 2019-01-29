6a266ad660ad9eadf4429f324a17f83de3f39d37
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-01-29 18:11:37 +0200

    Fix: Waiting for Auth Ready
    prevent app render to DOM until
    - firebase authentication is initialized
    - firebase can figure out we looged to app

--- src/index.js
+++ src/index.js
@@ -27,16 +27,26 @@ import fbConfig from './cofig/fbConfig';
 const store = createStore(rootReducer, 
     // use compose to combine store enhancers together
     // pass fbConfig file to firebase enhancer to connect project config file with our app
+    // pass config option {attachAuthIsReady: true} to allow us access -firebaseAuthIsReady- method on store 
+    // store.firebaseAuthIsReady allow us to prevent rendering to DOM until
+    // 1- firebase authentication is initialized
+    // 2- firebase can figure out we looged to app
+    // note: if your firebase reducers name is different than firebase
+    // you need to add reactReduxFirebase
+    // (fbConfig, {attachAuthIsReady:true, firebaseStateName:'yourUniqueReducerNameGoesHere'})
     compose(
         applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
-        reactReduxFirebase(fbConfig),
+        reactReduxFirebase(fbConfig, {attachAuthIsReady: true}),
         reduxFirestore(fbConfig)
     )
 );
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+// fire callback func(and render to DOM) after firebase authentication is initialized
+store.firebaseAuthIsReady.then(() => {
+    ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: http://bit.ly/CRA-PWA
+    serviceWorker.unregister();
+})
